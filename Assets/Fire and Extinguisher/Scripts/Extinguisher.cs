42d5dbafa7a3f5237e409f38b63141a43eb6c1f8
LilSpectrum <demiri.lazic.len@gmail.com>
2023-10-19 15:36:20 +0200

    Fire and Extinguisher
    :)

---
+++ Assets/Fire and Extinguisher/Scripts/Extinguisher.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Extinguisher : MonoBehaviour
+{
+    [SerializeField] private float amountExtinguishedPerSecond = 1.0f;
+    private void Update()
+    {
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 100f)
+            && hit.collider.TryGetComponent(out Fire fire))
+            fire.TryExtinguish(amountExtinguishedPerSecond * Time.deltaTime);
+    }
+}
